c79d089f21e27a83189842b7857fb357681470ec
lennartmureau <lennartmureau@hotmail.com>
2021-05-16 22:40:27 +0200

    Added color depth image support

--- src/depth_imager.cpp
+++ src/depth_imager.cpp
@@ -21,11 +21,13 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& msg) {
   uint32_t length = msg->width;
   
   //Create a buffer for the output image
-  uint8_t outputImage[IMAGE_WIDTH*IMAGE_HEIGHT];
+  uint8_t outputImage[IMAGE_WIDTH*IMAGE_HEIGHT * 3];
   //Clear the buffer to erase random data
   for (uint16_t xi = 0; xi < IMAGE_WIDTH; xi++) {
     for (uint16_t yi = 0; yi < IMAGE_HEIGHT; yi++) {
-      outputImage[xi + yi * IMAGE_WIDTH] = 0;
+      for (uint8_t c = 0; c < 3; c++) {
+        outputImage[(xi + yi * IMAGE_WIDTH) * 3 + c] = 0;
+      }
     }
   }
   
@@ -65,13 +67,42 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& msg) {
     //Calculate the distance from the point to the camera
     float dist = sqrt(x * x + y * y + z * z);
     
+    //Hue is the color from 0 to 360
+    uint16_t hue = dist * 60;
+    
+    uint8_t rgb[3] = {0, 0, 0};
+    
+    if (hue < 60) {
+      rgb[0] = 255;
+      rgb[1] = 255 * (hue / 60.0);
+    } else if (60 <= hue && hue < 120) {
+      rgb[1] = 255;
+      rgb[0] = 255 * (1 - (hue / 60.0) - 1);
+    } else if (120 <= hue && hue < 180) {
+      rgb[1] = 255;
+      rgb[2] = 255 * ((hue / 60.0) - 2);
+    } else if (180 <= hue && hue < 240) {
+      rgb[2] = 255;
+      rgb[1] = 255 * (1 - (hue / 60.0) - 3);
+    } else if (240 <= hue && hue < 300) {
+      rgb[2] = 255;
+      rgb[0] = 255 * ((hue / 60.0) - 4);
+    } else {
+      rgb[0] = 255;
+      rgb[2] = 255 * (1 - (hue / 60.0) - 5);
+    }
+    
     //TODO: Create a better way of displaying the pixels
     //Color the output pixel according to the distance
-    //outputImage[pixelX + pixelY * IMAGE_WIDTH] = dist * 50;
+    //outputImage[(pixelX + pixelY * IMAGE_WIDTH) * 3] = rgb[0];
+    //outputImage[(pixelX + pixelY * IMAGE_WIDTH) * 3 + 1] = rgb[1];
+    //outputImage[(pixelX + pixelY * IMAGE_WIDTH) * 3 + 2] = rgb[2];
     //Color an area surrounding the pixel (temporarely, for better viewing)
     for (int8_t offX = -1; offX <= 1; offX++) {
       for (int8_t offY = -5; offY <= 5; offY++) {
-        outputImage[pixelX + offX + (pixelY + offY) * IMAGE_WIDTH] = dist * 50;
+        outputImage[(pixelX + offX + (pixelY + offY) * IMAGE_WIDTH) * 3] = rgb[0];
+        outputImage[(pixelX + offX + (pixelY + offY) * IMAGE_WIDTH) * 3 + 1] = rgb[1];
+        outputImage[(pixelX + offX + (pixelY + offY) * IMAGE_WIDTH) * 3 + 2] = rgb[2];
       }
     }
   }
@@ -84,12 +115,12 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& msg) {
   outMsg.header = std_msgs::Header();
   outMsg.height = (IMAGE_HEIGHT);
   outMsg.width = (IMAGE_WIDTH);
-  outMsg.encoding = "mono8";
+  outMsg.encoding = "rgb8";
   //TODO: Check the endianess
   outMsg.is_bigendian = false;
-  outMsg.step = (IMAGE_WIDTH);
+  outMsg.step = (IMAGE_WIDTH * 3);
   //TODO: Optimize the following statement
-  std::vector<uint8_t> output(outputImage, outputImage + IMAGE_WIDTH * IMAGE_HEIGHT);
+  std::vector<uint8_t> output(outputImage, outputImage + IMAGE_WIDTH * IMAGE_HEIGHT * 3);
   outMsg.data = output;
   pub.publish(outMsg);
   
